1ec44bca798a2602e9dde966ad7e30a7b931f2ef
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-11 19:59:27 -0400

    edited configure_db.sh.

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -4,15 +4,15 @@ export PGPASSWORD='12345'
 
 echo "Configuring dragonstackdb..."
 
-dropdb dragonstackdb
-createdb dragonstackdb
+dropdb dragon-stack-db
+createdb dragon-stack-db
 
-psql dragonstackdb < ./bin/sql/account.sql
-psql dragonstackdb < ./bin/sql/generation.sql
-psql dragonstackdb < ./bin/sql/dragon.sql
-psql dragonstackdb < ./bin/sql/trait.sql
-psql dragonstackdb < ./bin/sql/dragonTrait.sql
-psql dragonstackdb < ./bin/sql/accountDragon.sql
+psql dragon-stack-db < ./bin/sql/account.sql
+psql dragon-stack-db < ./bin/sql/generation.sql
+psql dragon-stack-db < ./bin/sql/dragon.sql
+psql dragon-stack-db < ./bin/sql/trait.sql
+psql dragon-stack-db < ./bin/sql/dragonTrait.sql
+psql dragon-stack-db < ./bin/sql/accountDragon.sql
 
 node ./bin/insertTraits.js
 
