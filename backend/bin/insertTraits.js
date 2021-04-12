ceb9dc32238ba6412f27130f2618077fb10c9f7e
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-12 00:38:59 -0400

    edited insertTraits.js.

--- backend/bin/insertTraits.js
+++ backend/bin/insertTraits.js
@@ -1,19 +1,39 @@
-const pool = require("../databasePool");
-const TRAITS = require("../data/traits.json");
+// const pool = require("../databasePool");
+// const TRAITS = require("../data/traits.json");
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 TRAITS.forEach(TRAIT => {
     const traitType = TRAIT.type;
     const traitValues = TRAIT.values;
 
     traitValues.forEach(traitValue => {
-        pool.query(
+        client.query(
             `INSERT INTO trait("traitType", "traitValue") VALUES($1, $2) RETURNING id`,
             [traitType, traitValue],
             (error, response) => {
                 if(error) console.error(error);
                 const traitId = response.rows[0].id;
 
-                console.log(`Inserted trait - id: ${traitId}`)
+                console.log(`Inserted trait - id: ${traitId}`);
+                client.end();
             }
         )
     });
