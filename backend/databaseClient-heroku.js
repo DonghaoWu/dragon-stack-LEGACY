6801d50f2a412fdc3a1652ee7514d0cd380e4bf3
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-14 18:55:17 -0400

    changed Pool to Client to connect to postgreSQL.

--- backend/databaseClient-heroku.js
+++ backend/databaseClient-heroku.js
@@ -1,19 +1,19 @@
 const { Client } = require('pg');
 
 const client = new Client(
-    // {
-    //     connectionString: process.env.DATABASE_URL,
-    //     ssl: {
-    //         rejectUnauthorized: false
-    //     }
-    // }
     {
-        user: 'noah',
-        host: 'localhost',
-        database: 'dragonstackdb',
-        password: '12345',
-        port: 5432
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
+    // {
+    //     user: 'noah',
+    //     host: 'localhost',
+    //     database: 'dragonstackdb',
+    //     password: '12345',
+    //     port: 5432
+    // }
 );
 
 module.exports = client;
