3868b36af5f879f35fe2667ca2467947912d8610
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-13 23:54:01 -0400

    working on heroku postgre commands.

--- backend/bin/configure_db_heroku.sh
+++ backend/bin/configure_db_heroku.sh
@@ -2,6 +2,13 @@
 
 echo "Configuring dragonstackdb..."
 
-psql -U limnpmtwspaxbt dfkeu67p0gbhi3 < ./sql/account.sql
+heroku pg:reset DATABASE
+
+heroku pg:psql --app dragon-stack-2021 < ./bin/sql/account.sql
+heroku pg:psql --app dragon-stack-2021 < ./bin/sql/generation.sql
+heroku pg:psql --app dragon-stack-2021 < ./bin/sql/dragon.sql
+heroku pg:psql --app dragon-stack-2021 < ./bin/sql/trait.sql
+heroku pg:psql --app dragon-stack-2021 < ./bin/sql/dragonTrait.sql
+heroku pg:psql --app dragon-stack-2021 < ./bin/sql/accountDragon.sql
 
 echo "dragonstackdb configured!"
